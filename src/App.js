99113329d051ac77c4abe09da5fa6f879fa3b04b
munminwon <minwon9276@naver,com>
2024-11-18 17:04:40 +0900

    JSX_props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
-import CourseListCard from './compoments/course/CourseListCard';
+import Library from './compoments/step01/Library';
 
 
 // 리액트에서는 class 는 예약어 이기 때문에 사용 못하고,
 // class 대신 className 이라고 사용한다.
 function App() {
   return (
-    <main>
-
-   {/* <Header/>
-   <Main/>
-   <Footer/> */}
-
-      <CourseListCard />
-    </main>
-  );
+      <Library/>
+   );
 }
 
 export default App;
